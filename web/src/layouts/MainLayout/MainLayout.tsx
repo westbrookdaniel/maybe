4ce4e37af1e69aaef27ce1ab265099bdfdfe2f13
westy12dan <westy12dan@gmail.com>
2024-01-19 19:31:28 +0800

    homeish

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,26 @@
+import { Link, routes } from '@redwoodjs/router'
+
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <div>{children}</div>
+  return (
+    <div className="flex flex-col space-y-8">
+      <header className="flex p-8">
+        <div className="flex-1" />
+        <nav className="flex flex-1 justify-center space-x-8">
+          <Link to={routes.home()}>Keep</Link>
+          <Link to={routes.home()}>Maybe</Link>
+          <Link to={routes.home()}>Profile</Link>
+        </nav>
+        <div className="flex flex-1 justify-end">
+          {/* <input type="text" placeholder="Search..." /> */}
+        </div>
+      </header>
+      <main className="mx-auto w-full max-w-3xl">{children}</main>
+    </div>
+  )
 }
 
 export default MainLayout
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,33 +1,28 @@
 import { useAuth } from 'src/auth'
-import { Link, routes } from '@redwoodjs/router'
-import { Metadata } from '@redwoodjs/web'
+import ItemsCell from 'src/components/Item/ItemsCell'
 
 const HomePage = () => {
-  const { isAuthenticated, signUp, logIn, logOut, currentUser, userMetadata } =
-    useAuth()
+  const { isAuthenticated, signUp, logIn, logOut } = useAuth()
 
-  console.log({ currentUser, userMetadata })
+  if (!isAuthenticated) {
+    return (
+      <>
+        <p className="mt-32 text-center">Login to see your items</p>
+        <div className="mt-8 flex flex-col space-y-2">
+          <button className="button" onClick={() => logIn()}>
+            log in
+          </button>
+          <button className="button" onClick={() => signUp()}>
+            sign up
+          </button>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
-      <Metadata title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-      <p>
-        Items can be found at <Link to={routes.items()}>Items</Link>
-      </p>
-
-      <p>{JSON.stringify({ isAuthenticated })}</p>
-      <button onClick={() => signUp()}>sign up</button>
-      <button onClick={() => logIn()}>log in</button>
-      <button onClick={() => logOut()}>log out</button>
+      <ItemsCell />
     </>
   )
 }
