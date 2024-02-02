c52ea7c8e768cf999ad461e8c4e4ef430017671d
westy12dan <westy12dan@gmail.com>
2024-02-02 10:46:11 +0800

    logout

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { NavLink, routes } from '@redwoodjs/router'
+import { useAuth } from 'src/auth'
 
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const { logOut } = useAuth()
   return (
     <div
       className="flex min-h-screen flex-col space-y-8 bg-gray-100"
@@ -27,6 +29,18 @@ const MainLayout = ({ children }: MainLayoutProps) => {
           >
             Maybe
           </NavLink>
+          <button
+            onClick={() =>
+              logOut({
+                openUrl(url) {
+                  window.location.replace(url)
+                },
+              })
+            }
+            className="w-20 rounded-lg p-1 text-center text-gray-400"
+          >
+            Logout
+          </button>
         </nav>
         <div className="flex flex-1 justify-end">
           {/* <input type="text" placeholder="Search..." /> */}
